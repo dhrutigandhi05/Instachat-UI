9a2f56d46ab438a5d8fe8bdcf77d484fef1b00cb
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-01-13 21:59:29 -0500

    websocket connection

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import './App.css';
 import Join from './join';
 import WSConnector from './WSConnector';
 
+type Client = {
+  connectionId: string;
+  nickname: string;
+};
+
 const wsConnector = new WSConnector();
 
 function App() {
-  const [nickname, setNickname] = useState(window.localStorage.getItem("nickname") || "");
+  const [nickname, setNickname] = useState<string>(window.localStorage.getItem("nickname") || "");
   useEffect(() => {window.localStorage.setItem("nickname", nickname)});
 
   const wsConnectortRef = useRef(wsConnector);
@@ -16,8 +21,22 @@ function App() {
     return <Join setNickname={setNickname}/>
   }
 
-  const url = `wss://hnr10uqox5.execute-api.us-east-1.amazonaws.com/dev?$nickname=${nickname}`
+  const url = `wss://hnr10uqox5.execute-api.us-east-1.amazonaws.com/dev?$nickname=${nickname}`;
   const ws = wsConnectortRef.current.getConnection(url);
+  ws.onopen = () => {
+    ws.send(JSON.stringify({
+      action: "getClients",
+    })
+  )};
+
+  ws.onmessage = (e) => {
+    const message = JSON.parse(e.data) as {
+      type: string
+      value: {
+        clients: Client[]
+      };
+    };
+  };
   
 
   return (
