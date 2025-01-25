39aaaebe661c83e9ea09fb13c85cfb111203d6ad
dhrutigandhi05 <dhrutigandhi.05@gmail.com>
2025-01-24 21:09:38 -0500

    updating icon

--- src/Bar.tsx
+++ src/Bar.tsx
@@ -7,7 +7,7 @@ const Bar = () => {
                 <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="" className="w-8 rounded-full mr-2"/>
                 <span className="font-bold">Chats</span>
             </div>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-start">
                 <div className="flex mb-5">
                     <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="" className="w-10 mr-2 rounded-full"/>
                     <span className="text-sm leading-9">Friend 1</span>
